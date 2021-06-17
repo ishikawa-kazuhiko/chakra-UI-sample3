6e33982784529717ecc14f5855d3792f76a4cc88
ishikawa-kazuhiko <85629860+ishikawa-kazuhiko@users.noreply.github.com>
2021-06-17 19:06:43 +0900

    ユーザー詳細モーダル画面の作成途中 ユーザー詳細モーダル画面の作成途中

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,22 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {
-  Box,
   Center,
-  Image,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Stack,
-  Text,
+  useDisclosure,
   Wrap,
   WrapItem
 } from "@chakra-ui/react";
-import { memo, useEffect, VFC } from "react";
+import { memo, useCallback, useEffect, VFC } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
 
+  //初期マウント時にで１回だけgetUsersが実行
   useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <>
@@ -32,11 +44,44 @@ export const UserManagement: VFC = memo(() => {
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser} //なぜこうなるのか確認
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        // motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>名前</FormLabel>
+                <Input value="山田太郎" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>フルネーム</FormLabel>
+                <Input value="山田太郎" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Mail</FormLabel>
+                <Input value="aaa@aaa.co.jp" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Tel</FormLabel>
+                <Input value="000-0000-9999" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 });
