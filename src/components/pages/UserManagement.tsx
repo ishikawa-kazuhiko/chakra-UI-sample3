71dc592f9849f5151f688112e3f07bad9a0e3176
ishikawa-kazuhiko <85629860+ishikawa-kazuhiko@users.noreply.github.com>
2021-06-21 17:13:21 +0900

    管理者ユーザー編集機能 管理者ユーザーのみユーザー情報を編集する導線の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -60,7 +60,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
